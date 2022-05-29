99f5a19201d01ed619998c7922b31a4f16deb62f
devmilk <nottiestyle@gmail.com>
2022-05-29 23:19:36 +0900

    カードコンテンツを入れる。ReadMeにFigmaのカンプとHTMLサイトURLを追加。

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # DashBoad
-MyBootstrapOnly to React-Bootstrap
+Bootstrap to React-Bootstrap
+
+カンプ
+ https://www.figma.com/proto/QMb4snA8M7nWujb5JcSY6U/DashBoad?scaling=min-zoom&page-id=0%3A1&node-id=23%3A38
+
+HTML
+ https://dev-milk.github.io/MyBootstrapOnly/
--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,8 @@ import {
   Row,
   Col,
   Breadcrumb,
+  Card,
+  Button,
    } from 'react-bootstrap';
  import {LinkContainer} from "react-router-bootstrap";
  
@@ -15,16 +17,25 @@ function Home() {
   return (
     <div >
 <Container >
+  {/* パンくず */}
 <Breadcrumb>
 <LinkContainer to="/">
    <Breadcrumb.Item >Home</Breadcrumb.Item>
 </LinkContainer>
 </Breadcrumb>
-<Row  className="justify-content-md-center m-5">
-  
+
+<Row  className="g-5 mb-3">
     <Col > 
+    <Card className="rounded-0 grid-border">
+  <Card.Body>
+    <Card.Title className="text-center title-section">Notification</Card.Title>
+    {<div className="d-md-flex justify-content-between">
+    <Card.Img variant="top" src="/images/mail.png" />
+      ここにdiv入ります。</div>}
     
-     {<p>これはDashboadの内容です。</p>}
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
     </Col>
   </Row> 
 
