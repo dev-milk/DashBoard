4ff7d98d63fb943728c00c3d751401a7c1ca6541
devmilk <nottiestyle@gmail.com>
2022-05-31 08:13:10 +0900

    Homeにコンテンツ追加。各画面との接続。

--- src/Home.js
+++ src/Home.js
@@ -8,15 +8,18 @@ import {
   Breadcrumb,
   Card,
   Button,
+  Image,
+  ListGroup,
    } from 'react-bootstrap';
  import {LinkContainer} from "react-router-bootstrap";
+ import MailImg from "./images/mail.png"; // mail.png
  
  
 
 function Home() {
   return (
     <div >
-<Container >
+<Container id="top">
   {/* パンくず */}
 <Breadcrumb>
 <LinkContainer to="/">
@@ -24,22 +27,56 @@ function Home() {
 </LinkContainer>
 </Breadcrumb>
 
-<Row  className="g-5 mb-3">
+ {/* Notification */}
+<Row  className='g-5 mb-3'>
     <Col > 
-    <Card className="rounded-0 grid-border">
+    <Card className='rounded-0 grid-border'>
   <Card.Body>
-    <Card.Title className="text-center title-section">Notification</Card.Title>
-    {<div className="d-md-flex justify-content-between">
-    <Card.Img variant="top" src="/images/mail.png" />
-      ここにdiv入ります。</div>}
-    
-    <Button variant="primary">Go somewhere</Button>
+    <Card.Title className='text-center title-section'>Notification</Card.Title>
+
+    <div className='d-md-flex justify-content-between'>
+
+    <div className='flex-fill align-self-center'>
+    <Image src={MailImg}  className='d-block mx-auto img-fluid' />
+    </div>
+
+    <div className='flex-fill'>
+    <ListGroup variant="flush">
+  <ListGroup.Item>Support Server&emsp; Maintenance&emsp;7days ago</ListGroup.Item>
+  <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+  <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+  <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+</ListGroup>
+    </div>
+
+    <div className='flex-fill align-self-md-end  text-center pb-1'>
+    <LinkContainer to="/Transaction">
+    <Button variant='dark' className='btn-sm rounded-pill w-90 mt-5'>&gt;More</Button>
+    </LinkContainer>
+    </div>
+
+    </div>
+      
   </Card.Body>
 </Card>
     </Col>
   </Row> 
+  {/* Coupon */}
 
 </Container>
+<Row  className='g-5 mb-3'>
+    <Col > 
+    ここにクーポンコンテンツが入る。
+    </Col>
+  </Row>
+  {/* Modal */}
+
+  {/* Transaction / Schedule*/}
+  <Row  className='g-5 mb-3'>
+    <Col > 
+    ここに取引とスケジュールのコンテンツが入る。
+    </Col>
+  </Row>
     
   </div>
   );
